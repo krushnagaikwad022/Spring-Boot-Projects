59e8b028211898fe6b7a29651c07510b391dc702
gaikwadkrushna022-pixel <gaikwadkrushna022@gmail.com>
2025-12-17 22:59:12 +0530

    third commit

---
+++ IOCProj04-FactoryPattern-Solution22/src/main/java/com/nt/test/ProfessionalCustomer1.java
@@ -0,0 +1,15 @@
+package com.nt.test;
+
+import com.nt.comp.BudgetCar;
+import com.nt.comp.Car;
+
+public class ProfessionalCustomer1 {
+
+	public static void main(String[] args) {
+		
+		Car car=new BudgetCar("mh168498");
+		car.drive();
+
+	}
+
+}
